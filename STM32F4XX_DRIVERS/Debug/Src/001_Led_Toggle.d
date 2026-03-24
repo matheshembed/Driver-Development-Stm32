4103f0b9dc82cf78c2d08f95ae98d5f53446fd9c
matheshembed <matheshvarma25@gmail.com>
2026-03-24 21:22:59 +0530

    Removed STM32F4XX_DRIVERS folder

--- STM32F4XX_DRIVERS/Debug/Src/001_Led_Toggle.d
+++
@@ -1,7 +0,0 @@
-Src/001_Led_Toggle.o: ../Src/001_Led_Toggle.c \
- /home/mathesh/Driver-Development-Stm32/STM32F4XX_DRIVERS/DRIVERS/INC/STM32F446XX.h \
- /home/mathesh/Driver-Development-Stm32/STM32F4XX_DRIVERS/DRIVERS/INC/STM32F446XX_GPIO_DRIVER.h \
- /home/mathesh/Driver-Development-Stm32/STM32F4XX_DRIVERS/DRIVERS/INC/STM32F446XX.h
-/home/mathesh/Driver-Development-Stm32/STM32F4XX_DRIVERS/DRIVERS/INC/STM32F446XX.h:
-/home/mathesh/Driver-Development-Stm32/STM32F4XX_DRIVERS/DRIVERS/INC/STM32F446XX_GPIO_DRIVER.h:
-/home/mathesh/Driver-Development-Stm32/STM32F4XX_DRIVERS/DRIVERS/INC/STM32F446XX.h:
